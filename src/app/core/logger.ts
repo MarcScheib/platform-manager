8a77dfb867715f8a4e30ce8ccb5335fa9583eff6
Marc Scheib <marc.scheib@tech11.com>
2023-07-24 21:54:13 +0200

    fix: exchange logger to make mac builds work

--- src/app/core/logger.ts
+++ src/app/core/logger.ts
@@ -1,29 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-import { createLogger } from 'bunyan';
-import { getConfig } from './config';
+import log from 'electron-log';
 
-const dataConfig = getConfig();
-
-export interface LogStream {
-  path?: string;
-  stream?: NodeJS.WriteStream;
-}
-
-export interface LoggerConfig {
-  app: string;
-  name: string;
-  level: string;
-  streams: Array<LogStream>;
-}
-
-const loggerConfig: LoggerConfig = {
-  app: 'sqlectron-gui',
-  name: 'sqlectron-gui',
-  level: dataConfig.log.level,
-  streams: [],
-};
-
-const logger = createLogger(loggerConfig);
-
-export default (namespace: string): Console => logger.child({ namespace });
+export default (namespace: string) => log.scope(namespace);
